9cec1437253813ebfd517d71644118694cd63072
Onexkka <49119974+Onexkka@users.noreply.github.com>
2019-06-25 08:15:59 +0700

    test kmvc

---
+++ VacationManager.WEB/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using KnockoutMvcDemo.Controllers;
+using PerpetuumSoft.Knockout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VacationManager.WEB.Models;
+
+namespace VacationManager.WEB.Controllers
+{
+    public class HelloWorldController : KnockoutController
+    {
+        public ActionResult Index()
+        {
+            //InitializeViewBag("Hello world");
+            return View(new HelloWorldModel
+            {
+                FirstName = "Steve",
+                LastName = "Sanderson"
+            });
+        }
+    }
+}
